e3577f06d473b48e18d14ed86c1d404439665e6a
Ted Whitehead <ted.whitehead@threespot.com>
2018-03-30 10:19:53 -0400

    Add tests

--- test/index.test.js
+++ test/index.test.js
@@ -36,3 +36,29 @@ test('Custom options', () => {
       <img class="bg-image-source vh" src="https://satyr.io/320x16:9" srcset="https://satyr.io/320x16:9 320w, https://satyr.io/640x16:9 640w, https://satyr.io/980x16:9 980w" alt="Image description test.">
     </div>`));
 });
+
+
+test('No “bg-image-source” class', () => {
+  global.console = { warn: jest.fn() };
+
+  document.body.innerHTML = '<div class="bg-image"><img src="#" alt="no class"></div>';
+
+  var imageWrapper = document.querySelector(".bg-image");
+
+  new ObjectFitImage(imageWrapper);
+
+  expect(console.warn).toBeCalled();
+});
+
+
+test('No img tag', () => {
+  global.console = { warn: jest.fn() };
+
+  document.body.innerHTML = '<div class="bg-image"><p class="bg-image-source">Not an image</p></div>';
+
+  var imageWrapper = document.querySelector(".bg-image");
+
+  new ObjectFitImage(imageWrapper);
+
+  expect(console.warn).toBeCalled();
+});
